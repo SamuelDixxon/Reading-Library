ab7edded64be1ab79fca826e5e2e47a9422171b0
Samuel Dixon <samueldixon@tamu.edu>
2023-07-09 16:22:25 -0500

    Added all files

--- Folder-Utilities/rm.py
+++
@@ -1,9 +0,0 @@
-import os
-
-source_file = 'README.md'
-current_dir = os.getcwd()
-
-for root, dirs, files in os.walk(current_dir):
-    for file in files:
-        if file == source_file:
-            os.remove(os.path.join(root, file))
